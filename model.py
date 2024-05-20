bugfix: panelenergy units, InitVelocity & battery upper bound

model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 import config
 from solar import Solar
-from constraints import get_bounds, constraint_battery, objective, constraint_acceleration
+from constraints import get_bounds, constraint_battery, objective, constraint_acceleration, constraint_battery2
 
 EPSILON = 1e-8
 
@@ -19,13 +19,15 @@ class Motor:
 
     # def calculate_power(self, speed, acceleration, slope, dir, wind_speed, wind_dir):
     def calculate_power(self, speed, acceleration, slope):
+
         # Calculate power required to overcome rolling resistance and aerodynamic drag
         self.dynamic_speed_crr = (self.no_of_wheels / 3) * 4.1 * 10 ** (-5) * speed
         
         rolling_resistance = (self.mass * 9.8 * (self.zero_speed_crr + self.dynamic_speed_crr))  # Assume coefficient of friction = 0.01
         
         # drag_force = get_drag_force(self.CDA, speed, wind_speed, wind_dir, dir)
-        drag_force = 0.5 * self.CDA * config.AirDensity * (speed ** 2)
+        # drag_force = 0.5 * self.CDA * config.AirDensity * (speed ** 2)
+        drag_force = 0.5 * self.CDA * config.AirDensity * ((speed)*2+(12.57)* 2-2*(speed)*(12.57)*np.cos(np.radians(10)))
         
         power = (
             rolling_resistance + drag_force
@@ -33,7 +35,7 @@ class Motor:
             + self.mass * config.g * np.sin(slope)
         ) * abs(speed)
 
-        return power
+        return max(power, 0)
 
 
 class ElectricCar:
@@ -46,8 +48,7 @@ class ElectricCar:
         start_speed, stop_speed, dx, slope
     ):
         speed = (start_speed + stop_speed) / 2
-        if(dx == 0):
-            print("ture2")
+
         # instantaneous time elapsed 
         dt = self.calculate_dt(start_speed, stop_speed, dx)
 
@@ -57,7 +58,7 @@ class ElectricCar:
             slope
         )
         # Current energy consumption
-        energy_consumed = power * dt / 3600
+        energy_consumed = power * dt / 3600.0
 
         return (
             dt,
@@ -67,8 +68,6 @@ class ElectricCar:
         )
     
     def calculate_dt(self, start_speed, stop_speed, dx):
-        if (dx == 0 ):
-            print("ture")
         dt = 2 * dx /(start_speed + stop_speed + EPSILON)
         # print(dt)
         return dt
@@ -85,7 +84,7 @@ def main():
     solar_panel = Solar(config.PanelEfficiency, config.PanelArea)
 
     N_V = len(route_df) + 1
-    velocity_profile = np.ones(N_V) * 20
+    velocity_profile = np.ones(N_V) * config.InitialGuessVelocity
     RaceStartTime = 9 * 3600
 
     bounds = get_bounds(N_V)
@@ -96,6 +95,16 @@ def main():
             "args": (
                 car, solar_panel, route_df,
                 config.BatteryCapacity * (1 - config.DeepDischargeCap),
+                config.BatteryCapacity,
+                RaceStartTime)
+        },
+        {
+            "type": "ineq",
+            "fun": constraint_battery2,
+            "args": (
+                car, solar_panel, route_df,
+                config.BatteryCapacity * (1 - config.DeepDischargeCap),
+                config.BatteryCapacity,
                 RaceStartTime)
         },
         {
@@ -118,15 +127,22 @@ def main():
             'verbose': 3,
         }
     )
+    optimised_velocity_profile = optimised_velocity_profile.x
 
     print("done.")
-    print("Total time taken for race:", objective(np.array(optimised_velocity_profile.x), car, route_df), "\bs")
+    print("Total time taken for race:", objective(np.array(optimised_velocity_profile), car, route_df), "\bs")
     # print(optimised_velocity_profile)
 
     plt.plot(
         np.array(route_df['CumulativeDistance(km)']),
-        optimised_velocity_profile.x[:-1],
+        optimised_velocity_profile[:-1],
         label='Velocity Profile'
+    )
+    plt.plot(
+        np.array(route_df['CumulativeDistance(km)']),
+        [36,] * len(route_df['CumulativeDistance(km)']),
+        color='red',
+        linestyle='dashed'
     )
     plt.xlabel('Distance (km)')
     plt.ylabel('Velocity (m/s)')
@@ -135,6 +151,33 @@ def main():
     plt.grid(True)
     plt.show()
 
+    optimised_battery_profile = np.ones(len(optimised_velocity_profile)) * config.BatteryCapacity
+    cumE = 0
+    time_elapsed = 0
+
+    for i in range(len(optimised_velocity_profile)-1):
+        dt, dx, P, dE = car.drive_sim(
+            optimised_velocity_profile[i], optimised_velocity_profile[i+1],
+            route_df.iloc[i, 0], route_df.iloc[i, 2]
+        )
+
+        dE -= solar_panel.calculate_energy(dt, RaceStartTime+time_elapsed, route_df.iloc[0, 3], route_df.iloc[0, 4])
+        cumE += dE
+
+        time_elapsed += dt
+        optimised_battery_profile[i+1] -= cumE
+
+    plt.plot(
+        np.array(route_df['CumulativeDistance(km)']),
+        optimised_battery_profile[:-1] * 100 / config.BatteryCapacity,
+        label='Battery % Profile'
+    )
+    plt.xlabel('Distance (km)')
+    plt.ylabel('Battery (%)')
+    plt.title('Battery Profile')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     main()